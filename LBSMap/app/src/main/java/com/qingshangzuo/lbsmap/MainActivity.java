579d811028230352c48fa9343c1d0a4ea903a31e
luckilyswl <792411500@qq.com>
2019-07-27 13:45:49 +0800

    修改好了，可以显示地图了

--- LBSMap/app/src/main/java/com/qingshangzuo/lbsmap/MainActivity.java
+++ LBSMap/app/src/main/java/com/qingshangzuo/lbsmap/MainActivity.java
@@ -2,7 +2,6 @@ package com.qingshangzuo.lbsmap;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
